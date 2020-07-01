a2b18aef65263e7db00325e9fe2afb1ba05dbdc1
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-01 17:09:07 +0300

    Initial commit

---
+++ GSU.Museum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GSU.Museum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
