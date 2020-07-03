0bd58efa2e8e92cee84c712d9ce21425a5f61cf2
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-03 23:27:15 +0300

    Added model for getting database settings

---
+++ GSU.Museum.API/Data/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using GSU.Museum.API.Interfaces;
+
+namespace GSU.Museum.API.Data
+{
+    ///<inheritdoc/>
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
