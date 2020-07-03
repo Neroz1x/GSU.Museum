5a2e818a9127ec61a4cd7864f76cd0519ac3e571
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-03 23:47:40 +0300

    Added global excepton handler

---
+++ GSU.Museum.API/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,41 @@
+﻿using GSU.Museum.API.Data.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace GSU.Museum.API.Extensions
+{
+    /// <summary>
+    /// Global exception handler. Catches exceptions and send user error model
+    /// </summary>
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.HttpContext.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        if (contextFeature.Error.GetType() == typeof(Error))
+                        {
+                            Error error = contextFeature.Error as Error;
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(error));
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync(new Error() { ErrorCode = Errors.Unhandled_exception, Info = contextFeature.Error.Message }.ToString());
+                        }
+                    }
+                });
+            });
+        }
+    }
+}
