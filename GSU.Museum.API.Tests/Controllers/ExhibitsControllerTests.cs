3e8db1a04592d054024fae18c3cbf586510f5fdc
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-07 16:20:24 +0300

    Remove unnecessary namespaces

--- GSU.Museum.API.Tests/Controllers/ExhibitsControllerTests.cs
+++ GSU.Museum.API.Tests/Controllers/ExhibitsControllerTests.cs
@@ -7,11 +7,8 @@ using GSU.Museum.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace GSU.Museum.API.Tests.Controllers
