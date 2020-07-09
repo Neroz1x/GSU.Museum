e505429898044711e3e49dbdd543bc113cb08ce2
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-09 23:23:59 +0300

    Add UriBase

--- GSU.Museum/GSU.Museum/App.xaml.cs
+++ GSU.Museum/GSU.Museum/App.xaml.cs
@@ -6,11 +6,13 @@ namespace GSU.Museum
 {
     public partial class App : Application
     {
+        public const string UriBase = "10.0.2.2:5001";
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
