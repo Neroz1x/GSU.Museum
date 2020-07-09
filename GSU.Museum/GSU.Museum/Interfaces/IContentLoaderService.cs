6dcbe08a7f0b2d72d1587d319875861d6a1de1fe
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-09 23:26:37 +0300

    Add ContentLoaderService
    It makes requests to the API and then process responses and and return deserialized data

---
+++ GSU.Museum/GSU.Museum/Interfaces/IContentLoaderService.cs
@@ -0,0 +1,49 @@
+﻿using GSU.Museum.Shared.Data.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Shared.Interfaces
+{
+    /// <summary>
+    /// Makes request and retrieve data
+    /// </summary>
+    interface IContentLoaderService
+    {
+        /// <summary>
+        /// Send request to api/Halls
+        /// </summary>
+        /// <returns>List of halls without nested stands</returns>
+        Task<List<Hall>> LoadHalls();
+
+        /// <summary>
+        /// Send request to api/Halls/ID
+        /// </summary>
+        /// <param name="id">Id of the hall</param>
+        /// <returns>Hall and nested list of stands in this hall</returns>
+        Task<Hall> LoadHall(string id);
+
+        /// <summary>
+        /// Send request to api/Stands/hallId/id
+        /// </summary>
+        /// <param name="hallId">Id of the hall</param>
+        /// <param name="id">Id of the stand</param>
+        /// <returns>Stand</returns>
+        Task<Stand> LoadStand(string hallId, string id);
+
+        /// <summary>
+        /// Send request to api/Stands/hallId/standId/id
+        /// </summary>
+        /// <param name="hallId">Id of the hall</param>
+        /// <param name="standId">Id of the stand</param>
+        /// <param name="id">Id of the exhibit</param>
+        /// <returns>Exhibit</returns>
+        Task<Exhibit> LoadExhibit(string hallId, string standId, string id);
+
+        /// <summary>
+        /// Create HttpClient with ApiKey
+        /// </summary>
+        /// <returns>HttpClient with sertificate</returns>
+        HttpClient GetHttpClient();
+    }
+}
---
+++ GSU.Museum/GSU.Museum/Services/ContentLoaderService.cs
@@ -0,0 +1,109 @@
+﻿using GSU.Museum.Shared.Data.Models;
+using GSU.Museum.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using GSU.Museum.Shared.Services;
+
+[assembly: Dependency(typeof(ContentLoaderService))]
+namespace GSU.Museum.Shared.Services
+{
+    public class ContentLoaderService : IContentLoaderService
+    {
+        private string _language;
+
+        public ContentLoaderService()
+        {
+            switch (Thread.CurrentThread.CurrentUICulture.Name)
+            {
+                case "ru-RU":
+                    _language = "Ru";
+                    break;
+                case "en-US":
+                    _language = "En";
+                    break;
+                case "be-BY":
+                    _language = "Be";
+                    break;
+                default:
+                    _language = "En";
+                    break;
+            }
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            HttpClient httpClient;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    httpClient = new HttpClient(DependencyService.Get<IHTTPClientHandlerCreationService>().GetInsecureHandler());
+                    break;
+                default:
+                    ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                    httpClient = new HttpClient(new HttpClientHandler());
+                    break;
+            }
+
+            httpClient.DefaultRequestHeaders.Add("Language", _language);
+            httpClient.DefaultRequestHeaders.Add("X-API-KEY", "U3VwZXJTZWNyZXRBcGlLZXkxMjM");
+            return httpClient;
+        }
+
+        public async Task<Exhibit> LoadExhibit(string hallId, string standId, string id)
+        {
+            string content = await Load(new Uri($"https://{App.UriBase}/api/Exhibits/{hallId}/{standId}/{id}"));
+            Exhibit exhibit = JsonConvert.DeserializeObject<Exhibit>(content);
+            return exhibit;
+        }
+
+        public async Task<Hall> LoadHall(string id)
+        {
+            string content = await Load(new Uri($"https://{App.UriBase}/api/Halls/{id}"));
+            Hall hall = JsonConvert.DeserializeObject<Hall>(content);
+            return hall;
+        }
+
+        public async Task<List<Hall>> LoadHalls()
+        {
+            string content = await Load(new Uri($"https://{App.UriBase}/api/Halls"));
+            List<Hall> halls = JsonConvert.DeserializeObject<List<Hall>>(content);
+            return halls;
+        }
+
+        public async Task<Stand> LoadStand(string hallId, string id)
+        {
+            string content = await Load(new Uri($"https://{App.UriBase}/api/Stands/{hallId}/{id}"));
+            Stand stand = JsonConvert.DeserializeObject<Stand>(content);
+            return stand;
+        }
+
+        public async Task<string> Load(Uri uri)
+        {
+            HttpResponseMessage response = await GetHttpClient().GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            // if server-side exception
+            else if (response.StatusCode == HttpStatusCode.InternalServerError || response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var error = JsonConvert.DeserializeObject<Error>(content);
+                throw new Error() { Info = error.Info, ErrorCode = error.ErrorCode };
+            }
+            // if unhandled server-side exception
+            else
+            {
+                throw new Exception($"response status code: {response.StatusCode}");
+            }
+        }
+    }
+}
