afbe524128e8f7b75726a3f0573bb3e98473cb62
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-10 22:33:15 +0300

    Add main page

---
+++ GSU.Museum/GSU.Museum/Pages/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using GSU.Museum.Shared.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GSU.Museum
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel(Navigation);
+        }
+
+        async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            var viewModel = BindingContext as MainPageViewModel;
+            await viewModel.GetHalls();
+        }
+    }
+}
