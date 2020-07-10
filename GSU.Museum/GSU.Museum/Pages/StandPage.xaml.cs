11ef72125cbad7e97326a1766216336ec211a0f1
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-10 22:31:30 +0300

    Add page to display stand entity

---
+++ GSU.Museum/GSU.Museum/Pages/StandPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using GSU.Museum.Shared.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandPage : ContentPage
+    {
+        public StandPage(string hallId, string id)
+        {
+            InitializeComponent();
+            BindingContext = new StandPageViewModel(hallId, id, Navigation);
+        }
+
+        private async void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            var viewModel = BindingContext as StandPageViewModel;
+            await viewModel.GetExhibits();
+        }
+    }
+}
