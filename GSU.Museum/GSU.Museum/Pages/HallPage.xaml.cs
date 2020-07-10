cdb9748f188d8110dc0f77e8bae6353bb01f7e96
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-10 22:32:35 +0300

    Add page to display hall entity

---
+++ GSU.Museum/GSU.Museum/Pages/HallPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using GSU.Museum.Shared.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HallPage : ContentPage
+    {
+        public HallPage(string id)
+        {
+            InitializeComponent();
+            BindingContext = new HallPageViewModel(id, Navigation);
+        }
+
+        private async void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            var viewModel = BindingContext as HallPageViewModel;
+            await viewModel.GetStands();
+        }
+    }
+}
