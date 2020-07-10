cf9958aa56319376bcace6db483e6fd647feba47
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-10 22:30:27 +0300

    Add page to display exhibit

---
+++ GSU.Museum/GSU.Museum/Pages/ExhibitsArticle.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GSU.Museum.Shared.Data.Models;
+using GSU.Museum.Shared.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExhibitsArticle : ContentPage
+    {
+        public ExhibitsArticle(Exhibit exhibit)
+        {
+            InitializeComponent();
+            BindingContext = new ExhibitsArticleViewModel(exhibit, Navigation);
+        }
+
+        private async void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            var viewModel = BindingContext as ExhibitsArticleViewModel;
+            await viewModel.FillPage();
+        }
+    }
+}
