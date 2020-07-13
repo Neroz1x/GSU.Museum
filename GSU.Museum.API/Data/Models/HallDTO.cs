646173dd77b4930afc3bb05c3bcd1cb381eaf636
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-13 22:17:13 +0300

    Add methods to compute hash

--- GSU.Museum.API/Data/Models/HallDTO.cs
+++ GSU.Museum.API/Data/Models/HallDTO.cs
@@ -9,5 +9,73 @@ namespace GSU.Museum.API.Data.Models
         public bool State { get; set; }
         public byte[] Photo { get; set; }
         public List<StandDTO> Stands { get; set; }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = (int)2166136261;
+                hash = (hash * 16777619) ^ GetStringHashCode(Id);
+                hash = (hash * 16777619) ^ GetStringHashCode(Title);
+                hash = (hash * 16777619) ^ State.GetHashCode();
+                hash = (hash * 16777619) ^ GetByteHashCode(Photo);
+                if (Stands != null)
+                {
+                    foreach (var stand in Stands)
+                    {
+                        hash = (hash * 16777619) ^ (stand?.GetHashCode() ?? 1);
+                    }
+                }
+                return hash;
+            }
+        }
+
+        private int GetStringHashCode(string str)
+        {
+            if (str != null)
+            {
+                unchecked
+                {
+                    int hash1 = (5381 << 16) + 5381;
+                    int hash2 = hash1;
+
+                    for (int i = 0; i < str.Length; i += 2)
+                    {
+                        hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                        if (i == str.Length - 1)
+                        {
+                            break;
+                        }
+                        hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                    }
+
+                    return hash1 + (hash2 * 1566083941);
+                }
+            }
+            return 1;
+        }
+
+        private int GetByteHashCode(byte[] byteArray)
+        {
+            if (byteArray != null)
+            {
+                unchecked
+                {
+                    const int p = 16777619;
+                    int hash = (int)2166136261;
+
+                    for (int i = 0; i < byteArray.Length; i++)
+                        hash = (hash ^ byteArray[i]) * p;
+
+                    hash += hash << 13;
+                    hash ^= hash >> 7;
+                    hash += hash << 3;
+                    hash ^= hash >> 17;
+                    hash += hash << 5;
+                    return hash;
+                }
+            }
+            return 1;
+        }
     }
 }
--- GSU.Museum/GSU.Museum/Data/Models/Exhibit.cs
+++ GSU.Museum/GSU.Museum/Data/Models/Exhibit.cs
@@ -9,5 +9,73 @@ namespace GSU.Museum.Shared.Data.Models
         public string Text { get; set; }
         public bool State { get; set; }
         public List<byte[]> Photos { get; set; }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = (int)2166136261;
+                hash = (hash * 16777619) ^ GetStringHashCode(Id);
+                hash = (hash * 16777619) ^ GetStringHashCode(Title);
+                hash = (hash * 16777619) ^ GetStringHashCode(Text);
+                hash = (hash * 16777619) ^ State.GetHashCode();
+                if(Photos != null)
+                {
+                    foreach (var photo in Photos)
+                    {
+                        hash = (hash * 16777619) ^ GetByteHashCode(photo);
+                    }
+                }
+                return hash;
+            }
+        }
+
+        private int GetStringHashCode(string str)
+        {
+            if (str != null)
+            {
+                unchecked
+                {
+                    int hash1 = (5381 << 16) + 5381;
+                    int hash2 = hash1;
+
+                    for (int i = 0; i < str.Length; i += 2)
+                    {
+                        hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                        if (i == str.Length - 1)
+                        {
+                            break;
+                        }
+                        hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                    }
+
+                    return hash1 + (hash2 * 1566083941);
+                }
+            }
+            return 1;
+        }
+
+        private int GetByteHashCode(byte[] byteArray)
+        {
+            if (byteArray != null)
+            {
+                unchecked
+                {
+                    const int p = 16777619;
+                    int hash = (int)2166136261;
+
+                    for (int i = 0; i < byteArray.Length; i++)
+                        hash = (hash ^ byteArray[i]) * p;
+
+                    hash += hash << 13;
+                    hash ^= hash >> 7;
+                    hash += hash << 3;
+                    hash ^= hash >> 17;
+                    hash += hash << 5;
+                    return hash;
+                }
+            }
+            return 1;
+        }
     }
 }
