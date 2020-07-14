6b98fb72c8a14bcce07c6aec3796b315c94f9e53
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-15 00:31:18 +0300

    Add page to manage settings

---
+++ GSU.Museum/GSU.Museum/Pages/OptionsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using GSU.Museum.Shared.Services;
+using GSU.Museum.Shared.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OptionsPage : ContentPage
+    {
+        public OptionsPage()
+        {
+            InitializeComponent();
+            BindingContext = new OptionsPageViewModel(Navigation);
+        }
+
+        private async void ContentPage_Disappearing(object sender, System.EventArgs e)
+        {
+            await OnPageExit();
+        }
+
+        async Task OnPageExit()
+        {
+            await DependencyService.Get<CachingService>().WriteSettings();
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
