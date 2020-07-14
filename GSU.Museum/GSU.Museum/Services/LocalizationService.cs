dab2f147025172cadd2ccd627dc4a16ef48b85c7
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-15 00:27:26 +0300

    Add service to localize application
    It uses Language settings which are stored in Settings model in App.xaml.cs.
    
    Method Localize changes currunt UI language.

---
+++ GSU.Museum/GSU.Museum/Services/LocalizationService.cs
@@ -0,0 +1,46 @@
+﻿using GSU.Museum.Shared.Interfaces;
+using GSU.Museum.Shared.Services;
+using System.Threading;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocalizationService))]
+namespace GSU.Museum.Shared.Services
+{
+    public class LocalizationService : ILocalizationService
+    {
+        public void Localize()
+        {
+            switch (App.Settings.Language.CultureInfo.Name)
+            {
+                case "ru-RU":
+                    if (!Thread.CurrentThread.CurrentUICulture.Name.Equals("ru-RU"))
+                    {
+                        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru-RU");
+                        Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru-RU");
+                    }
+                    break;
+                case "en-US":
+                    if (!Thread.CurrentThread.CurrentUICulture.Name.Equals("en-US"))
+                    {
+                        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+                        Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                    }
+                    break;
+                case "be-BY":
+                    if (!Thread.CurrentThread.CurrentUICulture.Name.Equals("be-BY"))
+                    {
+                        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("be-BY");
+                        Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("be-BY");
+                    }
+                    break;
+                default:
+                    if (!Thread.CurrentThread.CurrentUICulture.Name.Equals("en-US"))
+                    {
+                        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+                        Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                    }
+                    break;
+            }
+        }
+    }
+}
