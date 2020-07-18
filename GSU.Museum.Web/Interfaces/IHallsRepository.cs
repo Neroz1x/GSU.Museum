ab3b7385957568e9f388d681403213af25795965
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-18 22:36:58 +0300

    Add repository to do CRUD operations with Hall

---
+++ GSU.Museum.Web/Interfaces/IHallsRepository.cs
@@ -0,0 +1,47 @@
+﻿using GSU.Museum.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Interfaces
+{
+    /// <summary>
+    /// Repository for model Hall
+    /// </summary>
+    public interface IHallsRepository
+    {
+        /// <summary>
+        /// Return all halls
+        /// </summary>
+        /// <returns></returns>
+        Task<List<HallViewModel>> GetAllAsync();
+
+        /// <summary>
+        /// Return hall by id
+        /// </summary>
+        /// <param name="id">Id of the hall</param>
+        /// <returns></returns>
+        Task<HallViewModel> GetAsync(string id);
+
+        /// <summary>
+        /// Add new record
+        /// </summary>
+        /// <param name="entity">New record to add</param>
+        /// <returns></returns>
+        Task CreateAsync(HallViewModel entity);
+
+        /// <summary>
+        /// Update existing record
+        /// </summary>
+        /// <param name="id">Record id</param>
+        /// <param name="entity">New record to update</param>
+        /// <returns></returns>
+        Task UpdateAsync(string id, HallViewModel entity);
+
+        /// <summary>
+        /// Delete record from database
+        /// </summary>
+        /// <param name="id">Id of the record</param>
+        /// <returns></returns>
+        Task RemoveAsync(string id);
+    }
+}
---
+++ GSU.Museum.Web/Repositories/HallsRepository.cs
@@ -0,0 +1,48 @@
+﻿using GSU.Museum.Web.Interfaces;
+using GSU.Museum.Web.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Repositories
+{
+    public class HallsRepository : IHallsRepository
+    {
+        private readonly IMongoCollection<HallViewModel> _halls;
+
+        public HallsRepository(DatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _halls = database.GetCollection<HallViewModel>(settings.CollectionName);
+        }
+
+        public async Task<List<HallViewModel>> GetAllAsync()
+        {
+            return await _halls.Find(exhibit => true).ToListAsync();
+        }
+
+        public async Task<HallViewModel> GetAsync(string id)
+        {
+            return await _halls.Find(exhibit => exhibit.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(HallViewModel exhibit)
+        {
+            await _halls.InsertOneAsync(exhibit);
+        }
+
+        public async Task UpdateAsync(string id, HallViewModel exhibitIn)
+        {
+            exhibitIn.Id = id;
+            await _halls.ReplaceOneAsync(exhibit => exhibit.Id.Equals(id), exhibitIn);
+        }
+
+        public async Task RemoveAsync(string id)
+        {
+            await _halls.DeleteOneAsync(exhibit => exhibit.Id.Equals(id));
+        }
+    }
+}
