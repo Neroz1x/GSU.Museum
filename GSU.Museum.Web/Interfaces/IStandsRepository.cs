29a34c10364da260bc74abf7f601fd6d0ae8d265
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-18 22:37:47 +0300

    Add repository to do CRUD operations with Stand

---
+++ GSU.Museum.Web/Interfaces/IStandsRepository.cs
@@ -0,0 +1,51 @@
+﻿using GSU.Museum.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Interfaces
+{
+    /// <summary>
+    /// Repository for entity Stand
+    /// </summary>
+    public interface IStandsRepository
+    {
+ 
+        /// <summary>
+        /// Returns list of stands in hall
+        /// </summary>
+        /// <param name="hallId">Id of hall</param>
+        /// <returns></returns>
+        Task<List<StandViewModel>> GetAllAsync(string hallId);
+
+        /// <summary>
+        /// Return stand by id
+        /// </summary>
+        /// <param name="hallId">Hall id</param>
+        /// <param name="id">Stand id</param>
+        /// <returns></returns>
+        Task<StandViewModel> GetAsync(string hallId, string id);
+
+        /// <summary>
+        /// Add new record
+        /// </summary>
+        /// <param name="entity">New record to add</param>
+        /// <returns></returns>
+        Task CreateAsync(string hallId, StandViewModel entity);
+
+        /// <summary>
+        /// Update existing record
+        /// </summary>
+        /// <param name="id">Record id</param>
+        /// <param name="entity">New record to update</param>
+        /// <returns></returns>
+        Task UpdateAsync(string hallId, string id, StandViewModel entity);
+
+        /// <summary>
+        /// Delete record from database
+        /// </summary>
+        /// <param name="id">Id of the record</param>
+        /// <returns></returns>
+        Task RemoveAsync(string hallId, string id);
+
+    }
+}
---
+++ GSU.Museum.Web/Repositories/StandsRepository.cs
@@ -0,0 +1,61 @@
+﻿using GSU.Museum.Web.Interfaces;
+using GSU.Museum.Web.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Repositories
+{
+    public class StandsRepository : IStandsRepository
+    {
+        private readonly IMongoCollection<HallViewModel> _halls;
+
+        public StandsRepository(DatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _halls = database.GetCollection<HallViewModel>(settings.CollectionName);
+        }
+
+        public async Task CreateAsync(string hallId, StandViewModel entity)
+        {
+            entity.Id = ObjectId.GenerateNewId().ToString();
+            var filter = Builders<HallViewModel>.Filter.Eq("Id", hallId);
+            var update = Builders<HallViewModel>.Update.Push("Stands", entity);
+
+            await _halls.UpdateOneAsync(filter, update);
+        }
+
+        public async Task<List<StandViewModel>> GetAllAsync(string hallId)
+        {
+            var hall = await _halls.Find(hall => hall.Id.Equals(hallId)).FirstOrDefaultAsync();
+            return hall?.Stands.ToList();
+        }
+
+        public async Task<StandViewModel> GetAsync(string hallId, string id)
+        {
+            var hall = await _halls.Find(hall => hall.Id.Equals(hallId)).FirstOrDefaultAsync();
+            return hall?.Stands.FirstOrDefault(stand => stand.Id.Equals(id));
+        }
+
+        public async Task RemoveAsync(string hallId, string id)
+        {
+            var update = Builders<HallViewModel>.Update.PullFilter(hall => hall.Stands,
+                                                stand => stand.Id.Equals(id));
+            var result = await _halls
+                .FindOneAndUpdateAsync(hall => hall.Id.Equals(hallId), update);
+        }
+
+        public async Task UpdateAsync(string hallId, string id, StandViewModel entity)
+        {
+            var arrayFilter = Builders<HallViewModel>.Filter.And(
+                Builders<HallViewModel>.Filter.Where(hall => hall.Id.Equals(hallId)),
+                Builders<HallViewModel>.Filter.Eq("Stands.Id", id));
+            var update = Builders<HallViewModel>.Update.Set("Stands.$", entity);
+
+            await _halls.UpdateOneAsync(arrayFilter, update);
+        }
+    }
+}
