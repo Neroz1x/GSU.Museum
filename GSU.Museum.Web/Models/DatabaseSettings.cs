d2e7cba2bfd6f5ef7d98d1b21ae4530683ceb279
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-18 22:24:26 +0300

    Add model to store database settings

---
+++ GSU.Museum.Web/Models/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using GSU.Museum.Web.Interfaces;
+
+namespace GSU.Museum.Web.Models
+{
+    ///<inheritdoc/>
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
