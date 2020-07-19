0204955deffdec3c4ea42eb5e65c69a7d7eacd7a
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-19 21:59:45 +0300

    Add models to store photo and info

---
+++ GSU.Museum.API/Data/Models/PhotoInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GSU.Museum.API.Data.Models
+{
+    public class PhotoInfoDTO
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
