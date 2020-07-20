c1e8c6349b1e8d2b80ae6047b9175946a9eecae2
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-20 23:06:13 +0300

    Add PhotoInfo model to replace byte array

---
+++ GSU.Museum/GSU.Museum/Data/Models/PhotoInfo.cs
@@ -0,0 +1,32 @@
+﻿namespace GSU.Museum.Shared.Data.Models
+{
+    public class PhotoInfo
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public byte[] Photo { get; set; }
+
+        public override int GetHashCode()
+        {
+            if (Photo != null)
+            {
+                unchecked
+                {
+                    const int p = 16777619;
+                    int hash = (int)2166136261;
+
+                    for (int i = 0; i < Photo.Length; i++)
+                        hash = (hash ^ Photo[i]) * p;
+
+                    hash += hash << 13;
+                    hash ^= hash >> 7;
+                    hash += hash << 3;
+                    hash ^= hash >> 17;
+                    hash += hash << 5;
+                    return hash;
+                }
+            }
+            return 1;
+        }
+    }
+}
