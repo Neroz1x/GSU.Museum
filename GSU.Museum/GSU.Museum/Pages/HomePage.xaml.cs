1f245ec8b86bfd6b4d141cc1610eaf4136ab45c0
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-27 23:13:37 +0300

    Add home page

---
+++ GSU.Museum/GSU.Museum/Pages/HomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using GSU.Museum.Shared.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        private bool _isActiveCarousel;
+        public HomePage()
+        {
+            InitializeComponent();
+            BindingContext = new HomePageViewModel(Navigation);
+        }
+
+        private void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            _isActiveCarousel = true;
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                if (Carousel.Position == 2)
+                {
+                    // Carousel.IsScrollAnimated = false;
+                    Carousel.Position = 0;
+                    // Carousel.IsScrollAnimated = true;
+                }
+                Carousel.Position += 1;
+                return _isActiveCarousel;
+            });
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+            _isActiveCarousel = false;
+        }
+    }
+}
---
+++ GSU.Museum/GSU.Museum/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GSU.Museum.Shared.Pages;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GSU.Museum.Shared.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        #region Fields
+        public INavigation Navigation;
+
+        public ObservableCollection<ImageSource> Images { get; set; } = new ObservableCollection<ImageSource>();
+        public Command NavigateToMainPageCommand { get; }
+        public Command NavigateToOptionsPageCommand { get; }
+
+        #endregion
+        public HomePageViewModel(INavigation navigation) 
+        {
+            Navigation = navigation;
+            NavigateToMainPageCommand = new Command(async () => await NavigateToMainPage());
+            NavigateToOptionsPageCommand = new Command(async () => await NavigateToOptionsPage());
+            Images.Add("Emblem.png");
+            Images.Add("Flag.jpeg");
+            Images.Add("Shield.jpeg");
+            Images.Add("Emblem.png");
+        }
+
+        #region Methods
+        private async Task NavigateToMainPage()
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async Task NavigateToOptionsPage()
+        {
+            await Navigation.PushAsync(new OptionsPage());
+        }
+        #endregion
+    }
+}
