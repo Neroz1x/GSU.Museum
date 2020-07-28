7ffe1d5931e275455238ab39d12fba3ebf3925b5
Ilya Grigoryev <Ilya.Grigoryev@outlook.com>
2020-07-29 00:05:23 +0300

    Change style in resources for menu item while loading app

--- GSU.Museum/GSU.Museum/Pages/LoadingPage.xaml.cs
+++ GSU.Museum/GSU.Museum/Pages/LoadingPage.xaml.cs
@@ -17,9 +17,22 @@ namespace GSU.Museum.Shared.Pages
         {
             if (App.Settings == null)
             {
-                App.Settings = await Xamarin.Forms.DependencyService.Get<CachingService>().ReadSettings();
-                Xamarin.Forms.DependencyService.Get<LocalizationService>().Localize();
+                App.Settings = await DependencyService.Get<CachingService>().ReadSettings();
+                DependencyService.Get<LocalizationService>().Localize();
             }
+            Style style = Application.Current.Resources["TransparentMenuItem"] as Style;
+            var width = Math.Round(App.Current.MainPage.Width - (0.1 * App.Current.MainPage.Width));
+            style.Setters.Add(new Setter() { Property = HeightRequestProperty, Value = width / 2 });
+            style.Setters.Add(new Setter() { Property = WidthRequestProperty, Value = width });
+            Application.Current.Resources.Remove("TransparentMenuItem");
+            Application.Current.Resources.Add("TransparentMenuItem", style);
+
+            style = Application.Current.Resources["DefaultMenuItem"] as Style;
+            style.Setters.Add(new Setter() { Property = HeightRequestProperty, Value = width / 2 });
+            style.Setters.Add(new Setter() { Property = WidthRequestProperty, Value = width });
+            Application.Current.Resources.Remove("DefaultMenuItem");
+            Application.Current.Resources.Add("DefaultMenuItem", style);
+
             App.Current.MainPage = new NavigationPage(new HomePage());
         }
     }
