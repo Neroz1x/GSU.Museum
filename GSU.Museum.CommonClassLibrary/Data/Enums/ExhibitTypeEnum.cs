de409d74edea7463f25df3f82b96853007b2fbf5
Neroz1x <neroz1x.post@gmail.com>
2020-08-25 15:17:14 +0300

    Add exhibit type enumeration

---
+++ GSU.Museum.CommonClassLibrary/Data/Enums/ExhibitTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSU.Museum.CommonClassLibrary.Data.Enums
+{
+    public enum ExhibitType
+    {
+        Article = 1,
+        Gallery
+    }
+}
