fa21faafe341e0c429d218facb09285849e47268
Neroz1x <neroz1x.post@gmail.com>
2020-08-27 15:55:13 +0300

    Add ExhibitGallery pages

---
+++ GSU.Museum/GSU.Museum/Pages/ExhibitGallery.xaml.cs
@@ -0,0 +1,26 @@
+﻿using GSU.Museum.CommonClassLibrary.Models;
+using GSU.Museum.Shared.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSU.Museum.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExhibitGallery : ContentPage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exhibit">Exhibit to display</param>
+        public ExhibitGallery(ExhibitDTO exhibit)
+        {
+            InitializeComponent();
+            BindingContext = new ExhibitGalleryViewModel(exhibit, Navigation);
+        }
+        private void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            var viewModel = BindingContext as ExhibitGalleryViewModel;
+            viewModel.FillPage();
+        }
+    }
+}
---
+++ GSU.Museum/GSU.Museum/ViewModels/ExhibitGalleryViewModel.cs
@@ -0,0 +1,69 @@
+﻿using GSU.Museum.CommonClassLibrary.Models;
+using GSU.Museum.Shared.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GSU.Museum.Shared.ViewModels
+{
+    public class ExhibitGalleryViewModel : BaseViewModel
+    {
+        #region Fields
+        public INavigation Navigation;
+        public Command NavigateToHomePageCommand { get; }
+        public ObservableCollection<PhotoInfoDTO> Photos { get; }
+
+        public ExhibitDTO Exhibit;
+
+        // Title of the page
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                }
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exhibit">Exhibit to display</param>
+        /// <param name="navigation">Instance of navigation</param>
+        /// <param name="navigation">Instance of navigation</param>
+        public ExhibitGalleryViewModel(ExhibitDTO exhibit, INavigation navigation)
+        {
+            Navigation = navigation;
+            Exhibit = exhibit;
+            Photos = new ObservableCollection<PhotoInfoDTO>();
+            NavigateToHomePageCommand = new Command(() => App.Current.MainPage = new NavigationPage(new HomePage()));
+        }
+
+        #region Methods
+        public void FillPage()
+        {
+            Photos.Clear();
+            foreach(var photo in Exhibit.Photos)
+            {
+                Photos.Add(photo);
+            }
+            Title = Exhibit.Title;
+        }
+        #endregion
+
+    }
+}
