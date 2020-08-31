d3bb3496b1ba00ba85ee77b5554f3905a79c3397
Neroz1x <neroz1x.post@gmail.com>
2020-08-31 19:39:46 +0300

    Redesign pages

--- GSU.Museum.Web/wwwroot/js/site.js
+++ GSU.Museum.Web/wwwroot/js/site.js
@@ -19,7 +19,6 @@ function ValidateImage() {
         }
     }
     else {
-        console.log($('#imageBlock').get()[0].children.length);
         if ($('#imageBlock').get()[0].children.length == 0) {
             $('#photoSpan').removeClass("field-validation-valid");
             $('#photoSpan').addClass("field-validation-error");
@@ -49,7 +48,7 @@ function readURL(input) {
             if (input.files[i]) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    $('#imageBlock').append("<div style='margin: 5px;'><img class='img-thumbnail small-image img-small-loaded' src='#'/><label style='text-align: center;'></label><input type='button' class='btn-outline-secondary btn-close' onclick='RemoveBlock(this)' value='x' /></div>");
+                    $('#imageBlock').append("<div style='margin: 5px;' class='ml-3'><img class='img-thumbnail small-image img-small-loaded' src='#'/><label style='text-align: center;'></label><input type='button' class='btn-outline-secondary btn-close' onclick='RemoveBlock(this)' value='x' /></div>");
                     $('#imageBlock').children().last().children().first().attr('src', e.target.result);
                     ValidateImage();
                 }
@@ -122,12 +121,36 @@ function LoadView(url) {
     });
 }
 
+function LoadCreateView(url) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+            $('#content').empty();
+            $('.selected-item').removeClass("selected-item");
+            $('#loader').css('visibility', 'visible');
+        },
+        success: function (view) {
+            $('#loader').css('visibility', 'collapse');
+            $('#content').html(view);
+            $("#form").removeData("validator");
+            $("#form").removeData("unobtrusiveValidation");
+            $.validator.unobtrusive.parse("#form");
+        }
+    });
+}
+
+
 // Send request to controller to perform delete and remove item from menu
 // url - url to send request
 // id - id of the recor to delete
-function DeleteItem(url, id) {
+// checkForEmtpy - check parent container to be emtpy after delete
+function DeleteItem(url, id, checkForEmtpy) {
     $('#deleteConfirm').attr('data-id', id);
     $('#deleteConfirm').attr('data-url', url);
+    $('#deleteConfirm').attr('data-checkForEmtpy', checkForEmtpy);
     $('#deleteConfirm').modal("show");
 }
 
@@ -188,7 +211,7 @@ function EditGallery(id, textId, url) {
 function Reorder(parendFrom, parentToId) {
     for (var i = parendFrom.children.length - 1; i >= 0; i--) {
         id = parendFrom.children[i].children[0].value;
-        $('#' + parentToId + ' > div[id=elId' + id + ']').insertAfter($('#' + parentToId + ' > a'));
+        $('#' + parentToId).prepend($('#' + parentToId + ' > div[id=elId' + id + ']'));
     }
 }
 
@@ -205,7 +228,7 @@ function CreateHall(textId) {
         processData: false,
         contentType: false,
         success: function (id) {
-            $('#halls').append("<div id='elId" + id + "'><div><div style='margin-top: 10px;' class='flex-row'><button class='btn btn-link collapsed' data-toggle='collapse' data-target='#id" + id + "' aria-expanded='false' aria-controls='id" + id + "' style='margin-bottom:3px;padding:2px;'><i class='fa' aria-hidden='false'></i></button><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Halls/Index/" + id + "')\">" + $('#' + textId).val() + "</a></div></div><div class='collapse' id='id" + id + "'><div><div style='margin-left: 50px'><div onclick=\"LoadViewFromMenu(this, '/Stands/Create?hallId=" + id + "')\"><img src='/images/add.png' class='img-btn' /><a class='a-btn' >Добавить стенд</a></div></div></div></div></div>");
+            $('#halls').append("<div id='elId" + id + "'><div><div style='margin-top: 10px;' class='flex-row'><button class='btn btn-link collapsed' data-toggle='collapse' data-target='#id" + id + "' aria-expanded='false' aria-controls='id" + id + "' style='margin-bottom:3px;padding:2px;visibility: hidden'><i class='fa' aria-hidden='false'></i></button><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Halls/Index/" + id + "')\">" + $('#' + textId).val() + "</a><button style='line-height:1em;' onclick=\"LoadCreateView('/Stands/Create?hallId=" + id + "')\" type='button' class='btn btn-secondary'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-plus-circle-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z' /></svg></button></div></div><div class='collapse' id='id" + id + "'></div></div>");
             $('#content').empty();
         }
     });
@@ -225,9 +248,9 @@ function CreateStand(hallId, textId) {
         processData: false,
         contentType: false,
         success: function (id) {
-            $('#id' + hallId).append("<div id='elId" + id + "'><div><div style='margin-top: 10px;margin-left: 50px' class='flex-row'><button class='btn btn-link collapsed' data-toggle='collapse' data-target='#id" + id + "' aria-expanded='false' aria-controls='id" + id + "' style='margin-bottom:3px;padding:2px;'><i class='fa' aria-hidden='false'></i></button><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Stands/Index/" + id + "?hallId=" + hallId + "')\">" + $('#' + textId).val() + "</a></div></div><div class='collapse' id='id" + id + "'><div><div style='margin-left: 100px'><div onclick=\"LoadViewFromMenu(this, '/Exhibits/Create?standId=" + id + "&hallId=" + hallId + "')\"><img src='/images/add.png' class='img-btn' /><a class='a-btn' >Добавить экспонат</a></div></div></div></div></div>");
+            $('#elId' + hallId).children().first().children().first().children().first().css("visibility", "visible");
+            $('#id' + hallId).append("<div id='elId" + id + "'><div><div style='margin-top: 10px;margin-left: 2em' class='flex-row'><button class='btn btn-link collapsed' data-toggle='collapse' data-target='#id" + id + "' aria-expanded='false' aria-controls='id" + id + "' style='margin-bottom:3px;padding:2px;visibility: hidden'><i class='fa' aria-hidden='false'></i></button><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Stands/Index/" + id + "?hallId=" + hallId + "')\">" + $('#' + textId).val() + "</a><button style='line-height:1em;' onclick=\"LoadCreateView('/Exhibits/Create?standId=" + id + "&hallId=" + hallId + "')\" type='button' class='btn btn-secondary'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-plus-circle-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z' /></svg></button></div></div><div class='collapse' id='id" + id + "'></div></div>");
             $('#content').empty();
-            $('.')
         }
     });
     $('.selected-item').removeClass("selected-item");
@@ -246,7 +269,8 @@ function CreateExhibit(hallId, standId, textId) {
         processData: false,
         contentType: false,
         success: function (id) {
-            $('#id' + standId).append("<div><div style='margin-top: 10px;margin-left: 100px;padding:2px;' class='flex-row' id='elId" + id + "'><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Exhibits/Index?id=" + id + "&standId=" + standId + "&hallId=" + hallId + "')\">" + $('#' + textId).val() +"</a ></div></div>");
+            $('#elId' + standId).children().first().children().first().children().first().css("visibility", "visible");
+            $('#id' + standId).append("<div style='margin-top: 10px;' class='flex-row' id='elId" + id + "'><div><div style='margin-left: 4em; padding:2px;'><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Exhibits/Index?id=" + id + "&standId=" + standId + "&hallId=" + hallId + "')\">" + $('#' + textId).val() + "</a></div></div></div>");
             $('#content').empty();
         }
     });
@@ -266,7 +290,8 @@ function CreateExhibitGallery(hallId, standId, textId) {
         processData: false,
         contentType: false,
         success: function (id) {
-            $('#id' + standId).append("<div><div style='margin-top: 10px;margin-left: 100px;padding:2px;' class='flex-row' id='elId" + id + "'><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Exhibits/Index?id=" + id + "&standId=" + standId + "&hallId=" + hallId + "')\">" + $('#' + textId).val() +"</a ></div></div>");
+            $('#elId' + standId).children().first().children().first().children().first().css("visibility", "visible");
+            $('#id' + standId).append("<div style='margin-top: 10px;' class='flex-row' id='elId" + id + "'><div><div style='margin-left: 4em; padding:2px;'><a class='a-header' id='elName" + id + "' onclick=\"LoadViewFromMenu(this, '/Exhibits/Index?id=" + id + "&standId=" + standId + "&hallId=" + hallId + "')\">" + $('#' + textId).val() + "</a></div></div></div>");
             $('#content').empty();
         }
     });
@@ -317,12 +342,12 @@ function ExhibitTypeChange(combobox, hallId, standId) {
 
 // Add new PhotoInfo part when creating exhibits galllery
 function AddPhotoInfo() {
-    $('#photos').append("<div><div><h5>Описание фотографии</h5><label>Фото</label><span class='field-validation-error'></span><div><div class='d-flex form-group'><button type='button' onclick='LoadPhoto(this)' class='btn btn-outline-secondary btn-form'><img src='/images/upload.png' class='img-btn' />Загрузить</button><input class='photoLoader' style='visibility: hidden' type='file' name='files' /></div></div></div><div class='form-group'><label for='descriptionBe'>Описание фото (BY)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на белорусском языке' name='photoDescriptionBe' class='form-control validation-unit' /></div><div class='form-group'><label for='descriptionRu'>Описание фото (RU)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на русском языке' name='photoDescriptionRu' class='form-control validation-unit' /></div><div class='form-group'><label for='descriptionEn'>Описание фото (EN)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на английском языке' name='photoDescriptionEn' class='form-control validation-unit' /></div><button type='button' onclick='DeletePhotoInfo(this)' class='btn btn-outline-secondary btn-form'><img src='/images/delete.png' class='img-btn' />Удалить запись</button></div></div>");
+    $('#photos').append("<div class='mt-3'><div><h5>Описание фотографии</h5><label class='ml-3'>Фото</label><span class='field-validation-error'></span><div class='ml-3'><div class='d-flex form-group ml-3'><button type='button' onclick='LoadPhoto(this)' class='btn btn-outline-secondary btn-form'><svg xmlns='http://www.w3.org/2000/svg' height='1.3em' viewBox='0 0 24 24' width='1.3em'><path fill='currentColor' d='M19 7v2.99s-1.99.01-2 0V7h-3s.01-1.99 0-2h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z' /></svg>Загрузить</button><input class='photoLoader' style='visibility: hidden' type='file' name='files' /></div></div></div><div class='form-group ml-3'><label for='descriptionBe'>Описание фото (BY)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на белорусском языке' name='photoDescriptionBe' class='form-control validation-unit ml-3' /></div><div class='form-group ml-3'><label for='descriptionRu'>Описание фото (RU)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на русском языке' name='photoDescriptionRu' class='form-control validation-unit ml-3' /></div><div class='form-group ml-3'><label for='descriptionEn'>Описание фото (EN)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на английском языке' name='photoDescriptionEn' class='form-control validation-unit ml-3' /></div><div class='form-group'><button type='button' onclick='DeletePhotoInfo(this)' class='btn btn-outline-secondary btn-form'><svg width='1.2em' height='1.2em' viewBox='0 0 16 16' class='bi bi-trash-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z'/></svg>Удалить запись</button></div></div></div>");
 }
 
 // Add new PhotoInfo part when editing exhibits galllery
 function AddPhotoInfoEdit() {
-    $('#photos').append("<div><div><h5>Описание фотографии</h5><label>Фото</label><span class='field-validation-error'></span><div><div class='d-flex form-group'><button type='button' onclick='LoadPhoto(this)' class='btn btn-outline-secondary btn-form'><img src='/images/upload.png' class='img-btn' />Загрузить</button><input class='photoLoaderEdit' style='visibility: hidden' type='file' name='files' /></div></div></div><div class='form-group'><label for='descriptionBe'>Описание фото (BY)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на белорусском языке' name='photoDescriptionBe' class='form-control validation-unit' /></div><div class='form-group'><label for='descriptionRu'>Описание фото (RU)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на русском языке' name='photoDescriptionRu' class='form-control validation-unit' /></div><div class='form-group'><label for='descriptionEn'>Описание фото (EN)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на английском языке' name='photoDescriptionEn' class='form-control validation-unit' /></div><button type='button' onclick='DeletePhotoInfo(this)' class='btn btn-outline-secondary btn-form'><img src='/images/delete.png' class='img-btn' />Удалить запись</button></div></div>");
+    $('#photos').append("<div class='mt-3'><div><h5>Описание фотографии</h5><label class='ml-3'>Фото</label><span class='field-validation-error'></span><div class='ml-3'><div class='d-flex form-group ml-3'><button type='button' onclick='LoadPhoto(this)' class='btn btn-outline-secondary btn-form'><svg xmlns='http://www.w3.org/2000/svg' height='1.3em' viewBox='0 0 24 24' width='1.3em'><path fill='currentColor' d='M19 7v2.99s-1.99.01-2 0V7h-3s.01-1.99 0-2h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z' /></svg>Загрузить</button><input class='photoLoaderEdit' style='visibility: hidden' type='file' name='files' /></div></div></div><div class='form-group ml-3'><label for='descriptionBe'>Описание фото (BY)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на белорусском языке' name='photoDescriptionBe' class='form-control validation-unit ml-3' /></div><div class='form-group ml-3'><label for='descriptionRu'>Описание фото (RU)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на русском языке' name='photoDescriptionRu' class='form-control validation-unit ml-3' /></div><div class='form-group ml-3'><label for='descriptionEn'>Описание фото (EN)</label><span class='field-validation-error'></span><input type='text' placeholder='Краткое описание фото на английском языке' name='photoDescriptionEn' class='form-control validation-unit ml-3' /></div><div class='form-group'><button type='button' onclick='DeletePhotoInfo(this)' class='btn btn-outline-secondary btn-form'><svg width='1.2em' height='1.2em' viewBox='0 0 16 16' class='bi bi-trash-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z'/></svg>Удалить запись</button></div></div></div>");
 }
 
 // Used to invoke method that display selected images
@@ -350,6 +375,13 @@ $(document).on("input", ".validation-unit", function () {
 
 // Is activated when confirm delete button pressed
 $(document).on("click", ".confirmOk", function () {
+    if ($('#deleteConfirm').attr('data-checkforemtpy') == "true") {
+        // Check parent container to have more children. If it is empty - make collapse button invisible
+        if ($('#elId' + $('#deleteConfirm').attr('data-id')).parent().children().length == 1) {
+            $('#elId' + $('#deleteConfirm').attr('data-id')).parent().parent().children().first().children().first().children().first().css("visibility", "hidden");
+        }
+    }
+
     $("#content").load($('#deleteConfirm').attr('data-url'));
     $('#elId' + $('#deleteConfirm').attr('data-id')).remove();
     $('#deleteConfirm').modal("hide");
@@ -381,7 +413,7 @@ function ReadPhoto(input) {
             if (input.files[i]) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    $(input).parent().parent().prepend("<div style='margin:0px 0px 10px 0px;'><img class='img-thumbnail small-image img-small-loaded' src='#'/><input type='button' class='btn-outline-secondary btn-close' onclick='RemovePhoto(this)' value='x' /></div>");
+                    $(input).parent().parent().prepend("<div style='margin:0px 0px 10px 0px;' class='ml-3'><img class='img-thumbnail small-image img-small-loaded' src='#'/><input type='button' class='btn-outline-secondary btn-close' onclick='RemovePhoto(this)' value='x' /></div>");
                     $(input).parent().parent().children().first().children().first().attr('src', e.target.result);
                 }
                 reader.readAsDataURL(input.files[i]);
