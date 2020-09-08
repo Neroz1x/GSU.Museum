c1abc401939d2fdbd77cf71e646e5189fcdb2e48
Neroz1x <neroz1x.post@gmail.com>
2020-09-08 16:25:33 +0300

    Add users repository

---
+++ GSU.Museum.Web/Interfaces/IUsersRepository.cs
@@ -0,0 +1,54 @@
+﻿using GSU.Museum.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Interfaces
+{
+    /// <summary>
+    /// Repository for model User
+    /// </summary>
+    public interface IUsersRepository
+    {
+        /// <summary>
+        /// Return all users
+        /// </summary>
+        /// <returns></returns>
+        Task<List<User>> GetAllAsync();
+
+        /// <summary>
+        /// Return user by login
+        /// </summary>
+        /// <param name="login">Login of the user</param>
+        /// <returns></returns>
+        Task<User> GetAsync(string login);
+        
+        /// <summary>
+        /// Return user by id
+        /// </summary>
+        /// <param name="id">Id of the user</param>
+        /// <returns></returns>
+        Task<User> GetByIdAsync(string id);
+
+        /// <summary>
+        /// Add new user
+        /// </summary>
+        /// <param name="user">New user to add</param>
+        /// <returns>An id of new user</returns>
+        Task<string> CreateAsync(User user);
+
+        /// <summary>
+        /// Update existing user
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <param name="user">New record to update</param>
+        /// <returns></returns>
+        Task UpdateAsync(string id, User user);
+
+        /// <summary>
+        /// Delete user by id
+        /// </summary>
+        /// <param name="id">Id of the user</param>
+        /// <returns></returns>
+        Task RemoveAsync(string id);
+    }
+}
---
+++ GSU.Museum.Web/Repositories/UsersRepository.cs
@@ -0,0 +1,55 @@
+﻿using GSU.Museum.CommonClassLibrary.Models;
+using GSU.Museum.Web.Interfaces;
+using GSU.Museum.Web.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GSU.Museum.Web.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UsersRepository(DatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _users = database.GetCollection<User>(settings.UsersCollectionName);
+        }
+
+        public async Task<string> CreateAsync(User user)
+        {
+            user.Id = ObjectId.GenerateNewId().ToString();
+            await _users.InsertOneAsync(user);
+            return user.Id;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _users.Find(user => true).ToListAsync();
+        }
+
+        public async Task<User> GetAsync(string login)
+        {
+            return await _users.Find(user => user.Login.Equals(login)).FirstOrDefaultAsync();
+        }
+        
+        public async Task<User> GetByIdAsync(string id)
+        {
+            return await _users.Find(user => user.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task RemoveAsync(string id)
+        {
+            await _users.DeleteOneAsync(user => user.Id.Equals(id));
+        }
+
+        public async Task UpdateAsync(string id, User user)
+        {
+            user.Id = id;
+            await _users.ReplaceOneAsync(user => user.Id.Equals(id), user);
+        }
+    }
+}
