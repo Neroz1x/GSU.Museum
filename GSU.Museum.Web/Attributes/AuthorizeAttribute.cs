7d8d73d1c41f582b111994914e288b0f1e135d3b
Neroz1x <neroz1x.post@gmail.com>
2020-09-08 16:19:12 +0300

    Add authentication attribute

---
+++ GSU.Museum.Web/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace GSU.Museum.Web.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var isAuthorised = context.HttpContext.Items["IsAuthorised"] ?? false;
+            if (!((bool)isAuthorised))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
