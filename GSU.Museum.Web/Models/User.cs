d41a73e88c0da247c58c54e06b10966edd05036a
Neroz1x <neroz1x.post@gmail.com>
2020-09-08 16:20:49 +0300

    Add user model

---
+++ GSU.Museum.Web/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace GSU.Museum.Web.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Введите логин")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
+        public string Password { get; set; }
+        public RefreshToken RefreshToken{ get; set; }
+    }
+}
