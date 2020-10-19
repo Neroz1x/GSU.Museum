53d59221fdbd76e40e8f5c1a1633628c95d3f096
Neroz1x <neroz1x.post@gmail.com>
2020-10-19 15:26:19 +0300

    Edit background photo
    Create different drawable resources for different screen dpi

--- GSU.Museum/GSU.Museum.Android/MainActivity.cs
+++ GSU.Museum/GSU.Museum.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using PanCardView.Droid;
 
 namespace GSU.Museum.Droid
 {
-    [Activity(Label = "GSU.Museum", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GSU Museum", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
