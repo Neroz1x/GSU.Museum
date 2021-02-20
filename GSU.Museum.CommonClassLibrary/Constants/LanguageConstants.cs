4cddbd6c2b1c00e782def3f9f2acb4ce79c8f3f0
Neroz1x <neroz1x.post@gmail.com>
2021-02-20 13:26:17 +0300

    Add constants for app languages

---
+++ GSU.Museum.CommonClassLibrary/Constants/LanguageConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace GSU.Museum.CommonClassLibrary.Constants
+{
+    public static class LanguageConstants
+    {
+        public static string LanguageRu = "ru";
+        public static string LanguageEn = "en";
+        public static string LanguageBy = "be";
+        
+        public static string LanguageFullRu = "ru-RU";
+        public static string LanguageFullEn = "en-US";
+        public static string LanguageFullBy = "be-BY";
+        
+        public static string LanguageDefault = "en";
+        public static string LanguageFullDefault = "en-US";
+    }
+}
