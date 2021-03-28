0800f10f3bd9f200687f0565e56af208c964c5c1
Neroz1x <neroz1x.post@gmail.com>
2021-03-28 19:16:18 +0300

    Remove unnecessary namespace using

--- GSU.Museum/GSU.Museum/Services/NetworkService.cs
+++ GSU.Museum/GSU.Museum/Services/NetworkService.cs
@@ -7,7 +7,6 @@ using GSU.Museum.Shared.Services;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
